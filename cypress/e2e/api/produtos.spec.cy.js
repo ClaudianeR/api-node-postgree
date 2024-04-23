f24613642ccb6ac9570ef12075c97915479866ad
CLAUDIANE MARIA GOMES  DE LIMA RODRIGUES <claudiane.guapa@gmail>
2024-04-22 21:51:07 -0300

    cypressconfig

--- cypress/e2e/api/produtos.spec.cy.js
+++ cypress/e2e/api/produtos.spec.cy.js
@@ -2,7 +2,7 @@
 
 describe ('GET retornando todos os produtos', () =>{
     it('GET /produtos', () =>{
-        cy.request ('http://127.0.0.1:5000/produtos')
+        cy.request ('/produtos')
         .its('status')
         .should('equal',200)
 
@@ -12,7 +12,7 @@ describe ('GET retornando todos os produtos', () =>{
 
 describe('GET retornando produto pelo id', () => {
     it('GET /produto/id', () => {
-        cy.request('http://127.0.0.1:5000/produto/1')
+        cy.request('/produto/1')
         .its('status')
         .should('equal',200)
 
